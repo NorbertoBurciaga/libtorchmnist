95a59d6c51dec22ca00e6de203aaf324f4617964
Norberto Burciaga <norberto.burciaga@nbsis.com>
2020-02-21 14:28:59 -0600

    Identify the device where torch is going to do calculations, CPU or GPU

--- source/applications/mnist/main.cpp
+++ source/applications/mnist/main.cpp
@@ -5,8 +5,21 @@
 
 using namespace std;
 
+torch::Device IdentifyDeviceType() {
+	if (torch::cuda::is_available()) {
+	    cout << "CUDA available! Training on GPU." << endl;
+	    return torch::kCUDA;
+	  } else {
+	    cout << "Training on CPU." << endl;
+	    return torch::kCPU;
+	  }
+}
+
 int main(int argc, char* argv[]) {
+	torch::Device device(IdentifyDeviceType());
+
 	cout << DATA_DIRECTORY << endl;
+
 	torch::Tensor tensor = torch::rand({2, 3});
 	cout << tensor << endl;
 	return 0;
